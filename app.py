ac101e2e0482b9f6e932553195f997da7fc902f9
Porter Support <93286801+portersupport@users.noreply.github.com>
2022-05-25 23:02:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     print(request.headers)
+    response.text("Check the console!")
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8080)
